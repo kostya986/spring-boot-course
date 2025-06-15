e5b76a4c5e265bc7665ae2a590ae17e458b92696
Konstantin Korkin <k.korkin@iteco-inno.ru>
2025-06-15 18:32:42 +0300

    Вернул - json

--- src/main/java/ru/kke/springbootcourse/model/ExternalInfo.java
+++ src/main/java/ru/kke/springbootcourse/model/ExternalInfo.java
@@ -1,12 +1,10 @@
 package ru.kke.springbootcourse.model;
 
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-@JacksonXmlRootElement
 public class ExternalInfo {
 
     private String info;
